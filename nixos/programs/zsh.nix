8c85a8bca2e23134c36e8cf51ce4e96c45e08166
Shenjunru <c2h5oc2h4@outlook.com>
2025-01-22 11:34:09 +0800

    flake

--- nixos/programs/zsh.nix
+++ nixos/programs/zsh.nix
@@ -15,9 +15,9 @@
     promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 
     shellAliases = {
-      nixos-test = "sudo nixos-rebuild test --flake '/data/nixos-config#Frbspc' --impure";
-      nixos-switch = "sudo nixos-rebuild switch --flake '/data/nixos-config#Frbspc' --impure";
-      nixos-update = "sudo nixos-rebuild switch --flake '/data/nixos-config#Frbspc' --impure --upgrade";
+      nixos-test = "sudo nixos-rebuild test --flake 'github:FrdrCkII/nixos-config#Frbspc' --impure";
+      nixos-switch = "sudo nixos-rebuild switch --flake 'github:FrdrCkII/nixos-config#Frbspc' --impure";
+      nixos-update = "sudo nixos-rebuild switch --flake 'github:FrdrCkII/nixos-config#Frbspc' --impure --upgrade";
     };
     
     ohMyZsh = {
