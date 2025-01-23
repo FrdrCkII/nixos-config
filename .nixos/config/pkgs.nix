9cb9d37c4d0d5548ca4711fdbde83230dd1e3454
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-23 10:01:54 +0800

    legacy configuration

--- .nixos/config/pkgs.nix
+++ .nixos/config/pkgs.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [ vscode ];
+  environment.systemPackages = with pkgs; [
+    microsoft-edge
+    vscode
+  ];
   programs.firefox = {
     enable = true;
     languagePacks = [ "zh-CN" ];
