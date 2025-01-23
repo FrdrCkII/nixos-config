3152a26dc0b6a052fa0da2e1bb9f13b4aecf1c59
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-23 12:19:58 +0800

    flake

--- system/config/filesystem.nix
+++ system/config/filesystem.nix
@@ -1,14 +1,13 @@
 { config, lib, pkgs, ... }:
 {
   fileSystems = {
-    "/".options = [ "subvol=@root" "noatime" "autodefrag" "compress=zstd:15" ];
-    "/nix".options = [ "subvol=@nix" "noatime" "autodefrag" "compress=zstd:15" ];
-    "/nix/store".options = [ "subvol=@nix/store" "noatime" "autodefrag" "compress=zstd:15" ];
-    "/nix/var".options = [ "subvol=@nix/var" "noatime" "autodefrag" "compress=zstd:15" ];
-    "/home".options = [ "subvol=@home" "noatime" "autodefrag" "compress=zstd:15" ];
-    "/swap".options = [ "subvol=@swap" "noatime" "autodefrag" "compress=zstd:15" ];
-    "/data".options = [ "subvol=@data" "noacl" "noatime" "autodefrag" "compress=zstd:15" ];
-    "/efi".options = [ "fmask=0022" "dmask=0022" ];
+    "/".options = [ "noatime" "autodefrag" "compress=zstd:15" ];
+    "/nix".options = [ "noatime" "autodefrag" "compress=zstd:15" ];
+    "/nix/store".options = [ "noatime" "autodefrag" "compress=zstd:15" ];
+    "/nix/var".options = [ "noatime" "autodefrag" "compress=zstd:15" ];
+    "/home".options = [ "noatime" "autodefrag" "compress=zstd:15" ];
+    "/swap".options = [ "noatime" "autodefrag" "compress=zstd:15" ];
+    "/data".options = [ "noacl" "noatime" "autodefrag" "compress=zstd:15" ];
   };
   swapDevices = [{
     device = "/swap/swapfile";
