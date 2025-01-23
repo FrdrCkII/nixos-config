48733144174beb6ddf383736361326509daea467
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-23 18:52:43 +0800

    flake

--- flake/programs/zsh.nix
+++ flake/programs/zsh.nix
@@ -5,20 +5,19 @@
   environment.pathsToLink = [ "/share/zsh" ];
   programs.zsh = {
     enable = true;
-    package = [ pkgs.zsh ];
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
-    plugins = [
-      {
-        name = "zimfw";
-        # src = pkgs.fetchFromGitHub {
-        #   owner = "marlonrichert";
-        #   repo = "zsh-autocomplete";
-        #   rev = "23.07.13";
-        #   sha256 = "sha256-/6V6IHwB5p0GT1u5SAiUa20LjFDSrMo731jFBq/bnpw=";
-        # };
-      }
-    ];
+    # plugins = [
+    #   {
+    #     name = "zimfw";
+    #     # src = pkgs.fetchFromGitHub {
+    #     #   owner = "marlonrichert";
+    #     #   repo = "zsh-autocomplete";
+    #     #   rev = "23.07.13";
+    #     #   sha256 = "sha256-/6V6IHwB5p0GT1u5SAiUa20LjFDSrMo731jFBq/bnpw=";
+    #     # };
+    #   }
+    # ];
   };
 }
