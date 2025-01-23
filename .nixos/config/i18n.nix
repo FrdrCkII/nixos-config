91dac85930a1fd2ab5e3b62ac3bc7aabd3c239a7
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-23 14:57:37 +0800

    flake

--- .nixos/config/i18n.nix
+++ .nixos/config/i18n.nix
@@ -12,6 +12,7 @@
     LC_PAPER = "zh_CN.UTF-8";
     LC_TELEPHONE = "zh_CN.UTF-8";
     LC_TIME = "zh_CN.UTF-8";
+    LC_ALL = "zh_CN.UTF-8";
   };
   fonts = lib.mkForce {
     enableDefaultPackages = false;
