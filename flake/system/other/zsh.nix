7ea98e2a70bd471461e9fc5c8a03921451c846d7
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-23 19:33:44 +0800

    flake

--- flake/system/other/zsh.nix
+++ flake/system/other/zsh.nix
@@ -9,7 +9,7 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    oh-my-zsh = {
+    ohMyZsh = {
       enable = true;
       plugins = [ "git" ];
     };
