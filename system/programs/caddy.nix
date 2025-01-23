7c13f55fef2359b5b1c3f15403835042d80f097b
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-23 15:04:24 +0800

    flake

--- system/programs/caddy.nix
+++ system/programs/caddy.nix
@@ -7,7 +7,7 @@
       description = "Steam & Github Reverse Proxy";
       serviceConfig = {
         WorkingDirectory = "/data/.tools/steamcommunity_302/";
-        ExecStart = ''./caddy run --config ./SteamDeck_302/steamcommunity_302.caddy.json --adapter caddyfile'';
+        ExecStart = ''/data/.tools/steamcommunity_302/caddy run --config /data/.tools/steamcommunity_302/SteamDeck_302/steamcommunity_302.caddy.json --adapter caddyfile'';
         Restart = ''on-failure'';
       };
     };
