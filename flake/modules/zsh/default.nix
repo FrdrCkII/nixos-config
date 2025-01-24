c7a8f8bdcd85a66da025928a9b55ca099c6f64f6
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-24 12:50:59 +0800

    zsh

--- flake/modules/zsh/default.nix
+++ flake/modules/zsh/default.nix
@@ -10,15 +10,15 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    plugins = [
-      {
-        name = "zimfw";
-        # file = "${pkgs.zimfw}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k";
-        file = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ]
+    # plugins = [
+    #   {
+    #     name = "zimfw";
+    #     # file = "${pkgs.zimfw}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    #   }
+    #   {
+    #     name = "powerlevel10k";
+    #     file = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    #   }
+    # ]
   };
 }
