86cd5be28d3e851fb4e406474956ec0f5e8eebad
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-24 12:43:14 +0800

    zsh

--- flake/modules/zsh/default.nix
+++ flake/modules/zsh/default.nix
@@ -1,6 +1,9 @@
 { config, pkgs, lib, username, ... }:
 {
-  home.packages = [ pkgs.zsh-powerlevel10k ];
+  home.packages = with pkgs; [
+    zsh zimfw
+    zsh-powerlevel10k
+  ];
   programs.zsh = {
     enable = true;
     dotDir = "../../dotfile/zsh";
@@ -8,9 +11,5 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    ohMyZsh = {
-      enable = true;
-      plugins = [ "git" ];
-    };
   };
 }
