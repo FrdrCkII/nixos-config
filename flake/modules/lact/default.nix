cc5fe13ec966f4c47e5b896b1b188e2f27b31553
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-24 13:16:52 +0800

    lact

--- flake/modules/lact/default.nix
+++ flake/modules/lact/default.nix
@@ -1,19 +1,22 @@
 { config, lib, pkgs, ... }:
 {
   home.packages = with pkgs; [ lact ];
-  systemd.user.services = {
-    lactd = {
-      Unit = {
-        Description = "AMDGPU Control Daemon";
-        After = ["multi-user.target"];
-      };
-      Service = {
-        ExecStart = "lact daemon";
-        Restart = "on-failure";
-        Nice = "-10";
-      };
-      Install = {
-        WantedBy = ["multi-user.target"];
+  systemd.user = {
+    enable = true;
+    services = {
+      lactd = {
+        Unit = {
+          Description = "AMDGPU Control Daemon";
+          After = ["multi-user.target"];
+        };
+        Service = {
+          ExecStart = "lact daemon";
+          Restart = "on-failure";
+          Nice = "-10";
+        };
+        Install = {
+          WantedBy = ["multi-user.target"];
+        };
       };
     };
   };
