2c2f883ce8e16cde98ac6f618d945f12e2be60e2
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-24 11:20:51 +0800

    i18n

--- flake/system/config/i18n.nix
+++ flake/system/config/i18n.nix
@@ -3,6 +3,8 @@
   time.timeZone = "Asia/Shanghai";
   i18n.defaultLocale = lib.mkForce "zh_CN.UTF-8";
   i18n.extraLocaleSettings = lib.mkForce {
+    LANG = "zh_CN.UTF-8";
+    LANGUAGE = "zh_CN.UTF-8";
     LC_ADDRESS = "zh_CN.UTF-8";
     LC_IDENTIFICATION = "zh_CN.UTF-8";
     LC_MEASUREMENT = "zh_CN.UTF-8";
@@ -12,7 +14,6 @@
     LC_PAPER = "zh_CN.UTF-8";
     LC_TELEPHONE = "zh_CN.UTF-8";
     LC_TIME = "zh_CN.UTF-8";
-    LC_ALL = "zh_CN.UTF-8";
   };
   fonts = lib.mkForce {
     enableDefaultPackages = false;
