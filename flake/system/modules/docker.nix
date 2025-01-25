e9d5c9042223eb81fab96a3727cb088daf2e7266
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-25 09:40:09 +0800

    docker

--- flake/system/modules/docker.nix
+++ flake/system/modules/docker.nix
@@ -7,7 +7,13 @@
     storageDriver = "btrfs";
     daemon.settings = {
       "registry-mirrors" = [
-        "https://docker.mirrors.ustc.edu.cn"
+        "https://docker.nastool.de"
+        "https://docker.1ms.run"
+        "https://docker.1panel.live"
+        "https://hub1.nat.tf"
+        "https://docker.1panel.top"
+        "https://dockerpull.org"
+        "https://docker.13140521.xyz"
         "https://registry.docker-cn.com"
       ];
     };
