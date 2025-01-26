55e8de9fad7659c0a659ef17290c192fb4201c58
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-26 19:03:57 +0800

    flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Frederick's NixOS Flake";
 
-  # outputs = inputs: import ./flakes inputs;
+  outputs = inputs: import ./flakes inputs;
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -17,65 +17,6 @@
     # NUR
     nur = {
       url = "github:nix-community/NUR";
-    };
-  };
-
-  outputs = 
-  {
-    self,
-    nixpkgs,
-    nixpkgs-stable,
-    nixpkgs-unstable,
-    home-manager,
-    nur,
-    ...
-  } @inputs: let
-    system = "x86_64-linux";
-    pkg-settings = import ./flakes/pkgs-settings.nix {
-      inherit system;
-      inherit nixpkgs;
-      inherit nixpkgs-stable;
-      inherit nixpkgs-unstable;
-      inherit nur;
-    };
-    hosts-conf = import ./flakes/hosts-conf.nix {
-      inherit pkg-settings;
-    };
-    system-gen = {host-conf}: with pkg-settings; nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = {
-        inherit allowed-unfree-packages;
-        inherit allowed-insecure-packages;
-        opt-config = host-conf.config;
-        hostname = host-conf.name;
-      };
-      modules = [
-        { nixpkgs.overlays = [ nur.overlay ]; }
-        ({
-          nixpkgs.overlays = [
-            (final: prev: {
-              stable = stable-pkgs;
-            })
-          ];
-        })
-        # System Configuration
-        ./user/${host-conf.config.username}/config.nix
-        # Home Manager
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${host-conf.config.username} = import ./user/${host-conf.config.username}/home.nix;
-          home-manager.extraSpecialArgs = {
-            inherit inputs;
-            opt-config = host-conf.config;
-          };
-        }
-      ];
-    };
-  in {
-    nixosConfigurations = with hosts-conf;{
-      "${Default.name}" = system-gen { host-conf = Default; };
-      "${c2h5oc2h4.name}" = system-gen { host-conf = c2h5oc2h4; };
     };
   };
 }
