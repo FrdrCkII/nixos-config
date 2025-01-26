53c6bcdc6fc7b0a46b5c6613816575eb139f1449
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-26 17:27:50 +0800

    flake

--- flakes/default.nix
+++ flakes/default.nix
@@ -18,8 +18,37 @@
   hosts-conf = import ./hosts-conf.nix {
     inherit pkg-settings;
   };
-  system-gen = import ./hosts-conf.nix {
-    inherit host-conf;
+  system-gen = with pkg-settings; nixpkgs.lib.nixosSystem {
+    inherit system;
+    specialArgs = {
+      inherit allowed-unfree-packages;
+      inherit allowed-insecure-packages;
+      opt-config = host-conf.config;
+      hostname = host-conf.name;
+      inherit custom-pkgs;
+    };
+    modules = [
+      { nixpkgs.overlays = [ nur.overlay ]; }
+      ({
+        nixpkgs.overlays = [
+          (final: prev: {
+            stable = stable-pkgs;
+          })
+        ];
+      })
+      # System Configuration
+      ../user/${host-conf.config.username}/config.nix
+      # Home Manager
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.${host-conf.config.username} = import ../user/${host-conf.config.username}/home.nix;
+        home-manager.extraSpecialArgs = {
+          inherit inputs;
+          opt-config = host-conf.config;
+        };
+      }
+    ];
   };
 in {
   nixosConfigurations = with hosts-conf;{
