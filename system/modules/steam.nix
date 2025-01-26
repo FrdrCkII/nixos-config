434ce3e66d0bdf2ab1bdc4a1def1f28b2001d893
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-26 20:53:25 +0800

    steam

--- system/modules/steam.nix
+++ system/modules/steam.nix
@@ -14,6 +14,40 @@
       localNetworkGameTransfers.openFirewall = true;
       extraCompatPackages = [ pkgs.proton-ge-bin ];
       protontricks.enable = true;
+      package = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+
+          # fix CJK fonts
+          source-sans
+          source-serif
+          source-han-sans
+          source-han-serif
+
+          # audio
+          pipewire
+
+          # other common
+          udev
+          alsa-lib
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr # To use the x11 feature
+          libxkbcommon
+          wayland # To use the wayland feature
+        ];
+      };
     };
     gamescope = {
       enable = true;
@@ -24,4 +58,7 @@
     steam-hardware.enable = true;
     xone.enable = true;
   };
+  fonts.packages = with pkgs; [
+    wqy_zenhei # Need by steam for Chinese
+  ];
 }
