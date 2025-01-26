2c5623b15d82ff741e16e4e47006dda868eb02c2
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-26 22:12:06 +0800

    i18n

--- system/config/i18n.nix
+++ system/config/i18n.nix
@@ -4,7 +4,8 @@
   i18n.defaultLocale = "zh_CN.UTF-8";
   i18n.extraLocaleSettings = {
     LANG = "zh_CN.UTF-8";
-    LC_CTYPE = "zh_CN.UTF-8";
+    LANGUAGE = "zh_CN:en_US";
+    LC_CTYPE = "en_US.UTF-8";
     LC_NUMERIC = "zh_CN.UTF-8";
     LC_TIME = "zh_CN.UTF-8";
     LC_COLLATE = "zh_CN.UTF-8";
