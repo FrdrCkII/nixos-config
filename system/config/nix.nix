c661d62cb22994ec866be5fcfc6c71b8e77d03d2
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-27 10:35:56 +0800

    flake

--- system/config/nix.nix
+++ system/config/nix.nix
@@ -10,11 +10,5 @@
     builders-use-substitutes = true;
     trusted-users = [ "${opt-config.username}" ];
     experimental-features = [ "nix-command" "flakes" ];
-    substituters = [
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://cache.nixos.org"
-    ];
   };
 }
