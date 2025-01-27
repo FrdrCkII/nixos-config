f22d465569a009ca8dfa050b39d2399f593f6da5
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-27 14:53:15 +0800

    docker

--- system/modules/docker.nix
+++ system/modules/docker.nix
@@ -1,35 +1,15 @@
 { config, lib, pkgs, opt-config, ... }:
 {
-  # users.users.${opt-config.username}.extraGroups = [ "docker" ];
+  users.users.${opt-config.username}.extraGroups = [ "docker" ];
   virtualisation = {
-    # docker = {
-    #   enable = true;
-    #   enableOnBoot = true;
-    #   storageDriver = "btrfs";
-    #   daemon.settings = {
-    #     "registry-mirrors" = [
-    #       "https://docker.m.daocloud.io"
-    #     ];
-    #   };
-    # };
-    podman = {
+    docker = {
       enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
-      autoPrune = {
-        enable = true;
-        dates = "weekly";
-        flags = ["--all"];
-      };
-    };
-    oci-containers = {
-      backend = "podman";
-      containers = {
-        # container-name = {
-        #   image = "container-image";
-        #   autoStart = true;
-        #   ports = [ "127.0.0.1:1234:1234" ];
-        # };
+      enableOnBoot = true;
+      storageDriver = "btrfs";
+      daemon.settings = {
+        "registry-mirrors" = [
+          "https://docker.m.daocloud.io"
+        ];
       };
     };
   };
