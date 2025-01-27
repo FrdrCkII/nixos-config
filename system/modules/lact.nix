89712da6ee20747daadb38e4fc0b66cb8857f234
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-27 10:10:17 +0800

    lact

--- system/modules/lact.nix
+++ system/modules/lact.nix
@@ -1,16 +1,6 @@
 { config, lib, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [ lact ];
-  systemd.services = {
-    lactd = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "multi-user.target" ];
-      description = "AMDGPU Control Daemon";
-      serviceConfig = {
-        ExecStart = ''${pkgs.lact}/bin/lact daemon'';
-        Restart = ''on-failure'';
-        Nice = ''-10'';
-      };
-    };
-  };
+  systemd.packages = with pkgs; [ lact ];
+  systemd.services.lactd.wantedBy = ["multi-user.target"];
 }
