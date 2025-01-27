1d804b2b08c6023db80ebc4bd8a2b05bfea94d24
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-27 12:03:00 +0800

    zsh

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -21,13 +21,8 @@
       (''
         ZIM_CONFIG_FILE=~/.config/zsh/.zimrc
         ZIM_HOME=~/.config/zsh/plugins/zimfw
-        # # Download zimfw plugin manager if missing.
-        # if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-        #   mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh \
-        #       https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-        # fi
         # Install missing modules and update ${ZIM_HOME}/init.zsh if missing or outdated.
-        if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]]; then
+        if [[ ! ${ZIM_HOME}/init.zsh -nt ~/.config/zsh/.zimrc} ]]; then
           source ${ZIM_HOME}/zimfw.zsh init -q
         fi
         # Initialize modules.
