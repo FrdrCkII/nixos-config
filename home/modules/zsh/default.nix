770dd691c5252a85b406838bebf895fd30bdf71e
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-27 12:18:33 +0800

    zsh

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -5,31 +5,26 @@
     zimfw
   ];
   home.file.".config/zsh/.zimrc".source = ./zimrc;
+  home.file.".config/zsh/zimfw/zimfw.zsh".source = ${pkgs.zimfw}/zimfw.zsh;
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     dotDir = ".config/zsh";
-    plugins = [
-      {
-        name = "zimfw";
-        src = pkgs.zimfw;
-      }
-    ];
     initExtra = let
       ZIM_CONFIG_FILE = "~/.config/zsh/.zimrc";
-      ZIM_HOME = "~/.config/zsh/plugins/zimfw";
+      ZIM_HOME = "~/.config/zsh/zimfw";
     in lib.mkMerge [
       (''
-        ZIM_CONFIG_FILE=~/.config/zsh/.zimrc
-        ZIM_HOME=~/.config/zsh/plugins/zimfw
-        # Install missing modules and update ~/.config/zsh/plugins/zimfw/init.zsh if missing or outdated.
-        if [[ ! ~/.config/zsh/plugins/zimfw/init.zsh -nt ~/.config/zsh/.zimrc ]]; then
-          source ~/.config/zsh/plugins/zimfw/zimfw.zsh init -q
+        ZIM_CONFIG_FILE=${ZIM_CONFIG_FILE}
+        ZIM_HOME=${ZIM_HOME}
+        # Install missing modules and update ${ZIM_HOME}/init.zsh if missing or outdated.
+        if [[ ! ${ZIM_HOME}init.zsh -nt ${ZIM_CONFIG_FILE} ]]; then
+          source ${ZIM_HOME}/zimfw.zsh init -q
         fi
         # Initialize modules.
-        source ~/.config/zsh/plugins/zimfw/init.zsh
+        source ${ZIM_HOME}/init.zsh
       '')
     ];
   };
