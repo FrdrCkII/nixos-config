6c835f6ed0ae16c207ae666ce0e7a1c0101e203e
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-27 11:30:18 +0800

    zim

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -4,7 +4,7 @@
     zsh
     zimfw
   ];
-  # home.file.".config/zsh/.zshrc".source = ./.zshrc;
+  home.file.".config/zsh/.zimrc".source = ./zimrc;
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -17,5 +17,20 @@
         src = pkgs.zimfw;
       }
     ];
+    initExtra = ''
+      ZIM_CONFIG_FILE=~/.config/zsh/.zimrc
+      ZIM_HOME=~/.zim
+      # Download zimfw plugin manager if missing.
+      if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+        mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh \
+            https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+      fi
+      # Install missing modules and update ${ZIM_HOME}/init.zsh if missing or outdated.
+      if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]]; then
+        source ${ZIM_HOME}/zimfw.zsh init -q
+      fi
+      # Initialize modules.
+      source ${ZIM_HOME}/init.zsh
+    ''
   };
 }
