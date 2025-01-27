36866e9281a09099b5d9a70eb9dce7f95bd90905
FrdrCkII <c2h5oc2h4@outlook.com>
2025-01-27 11:50:34 +0800

    zsh

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -20,7 +20,18 @@
     initExtra = lib.mkMerge [
       (''
         ZIM_CONFIG_FILE=~/.config/zsh/.zimrc
-        ZIM_HOME=~/.zim
+        ZIM_HOME=~/.config/zsh/plugins/zimfw
+        # # Download zimfw plugin manager if missing.
+        # if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+        #   mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh \
+        #       https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+        # fi
+        # Install missing modules and update ${ZIM_HOME}/init.zsh if missing or outdated.
+        if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]]; then
+          source ${ZIM_HOME}/zimfw.zsh init -q
+        fi
+        # Initialize modules.
+        source ${ZIM_HOME}/init.zsh
       '')
     ];
   };
